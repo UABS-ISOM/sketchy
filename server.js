500b5d611d5f28ab0ae6a5c847b7311607cd114c
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-11-23 16:25:32 +1300

    Create endpoint for drawing

--- server.js
+++ server.js
@@ -7,10 +7,38 @@ const logger = require("morgan");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+io.on("connection", function(socket) {
+  socket.on("draw", (x, y) => {
+    io.emit("drawBroadcast", x, y);
+  });
+  // socket.on('soundUpdate', function (data) {
+  //     let newData = {
+  //         latitude: data.latitude,
+  //         longitude: data.longitude,
+  //         decibels: data.decibels
+  //     }
+  //     for (let [index, o] of heatmapData.entries()) {
+  //         if (o.latitude == newData.latitude && o.longitude == newData.longitude) {
+  //             heatmapData.splice(index);
+  //         }
+  //     }
+  //     heatmapData.push(newData);
+  //     io.emit('soundBroadcast', {
+  //         max: 70,
+  //         data: heatmapData
+  //     });
+  // });
+});
 
 const port = process.env.PORT || 3000;
 
