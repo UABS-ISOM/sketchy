668b999033c6b6d0fddd593efe9ccac7856a3ead
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-11-24 17:23:23 +1300

    Create drawing client

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = require("express")();
+const path = require("path");
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const app = express();
-
 app.use(express.static("public"));
+app.use("/scripts", [
+  express.static(path.join(__dirname, "/node_modules/altspace/dist/")),
+  express.static(path.join(__dirname, "/node_modules/three/build"))
+]);
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,26 +22,9 @@ app.get("/", function(req, res) {
 });
 
 io.on("connection", function(socket) {
-  socket.on("draw", (x, y) => {
-    io.emit("drawBroadcast", x, y);
+  socket.on("draw", plots => {
+    socket.broadcast.emit("drawBroadcast", plots);
   });
-  // socket.on('soundUpdate', function (data) {
-  //     let newData = {
-  //         latitude: data.latitude,
-  //         longitude: data.longitude,
-  //         decibels: data.decibels
-  //     }
-  //     for (let [index, o] of heatmapData.entries()) {
-  //         if (o.latitude == newData.latitude && o.longitude == newData.longitude) {
-  //             heatmapData.splice(index);
-  //         }
-  //     }
-  //     heatmapData.push(newData);
-  //     io.emit('soundBroadcast', {
-  //         max: 70,
-  //         data: heatmapData
-  //     });
-  // });
 });
 
 const port = process.env.PORT || 3000;
