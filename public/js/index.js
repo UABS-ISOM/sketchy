088d109f038b9bf714e4a373b6da726bb4295fd0
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-11-30 12:26:23 +1300

    Add default color

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,14 @@
 const whiteBoardComponents = document.getElementById("white-board").components["drawable-canvas"];
-const colorPickers = document.getElementById("pen-tools").getElementsByTagName("a-circle");
+const colorPickers = document.getElementById("pen-tools").getElementsByClassName("color")
 const tick = document.getElementById("tick");
 
 for (colorPicker of colorPickers) {
-  console.log(colorPicker.object3D.el.appendChild)
+  if (colorPicker.attributes.color.value === whiteBoardComponents.penColor) {
+    const [x, y, z] = colorPicker.object3D.position.toArray()
+    tick.object3D.position.set(x,y,z+0.1)
+  }
   colorPicker.object3D.addEventListener("cursordown", function(){
     whiteBoardComponents.penColor = this.el.attributes.color.value;
-    this.el.appendChild(tick)
     tick.object3D.position.set(-0.05,-0.05,0.45)
   });
 }
