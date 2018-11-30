94bbb010abc03a6ab2062412f9477756301a74bb
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-11-30 14:05:22 +1300

    Added pen tools

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 const whiteBoardComponents = document.getElementById("white-board").components["drawable-canvas"];
-const colorPickers = document.getElementById("pen-tools").getElementsByClassName("color");
-const tick = document.getElementById("tick");
+const penTools = document.getElementById("pen-tools");
+const colorPickers = penTools.getElementsByClassName("color");
+const eraserButton = penTools.querySelector(".eraser");
+const penWidthIncreaseButton = penTools.querySelector(".increase-pen-width");
+const penWidthDecreaseButton = penTools.querySelector(".decrease-pen-width");
+const tick = penTools.querySelector(".tick");
 
 for (colorPicker of colorPickers) {
   if (colorPicker.attributes.color.value === whiteBoardComponents.penColor) {
@@ -13,3 +17,18 @@ for (colorPicker of colorPickers) {
     tick.object3D.position.set(x, y, z + 0.01);
   });
 }
+
+eraserButton.object3D.addEventListener("cursordown", function() {
+  console.log("erase")
+  whiteBoardComponents.penColor = "white";
+});
+
+penWidthIncreaseButton.object3D.addEventListener("cursordown", function() {
+  console.log("+")
+  if (whiteBoardComponents.penWidth < 30) whiteBoardComponents.penWidth += 1;
+});
+
+penWidthDecreaseButton.object3D.addEventListener("cursordown", function(){
+  console.log("-")
+  if (whiteBoardComponents.penWidth > 1) whiteBoardComponents.penWidth -= 1;
+});
