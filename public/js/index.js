32daf1f9de3e70e2991c00e1354543eedf215a0f
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-11-30 13:04:16 +1300

    Change checkmark image to geometry dot as altspace has bug with image

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,15 @@
 const whiteBoardComponents = document.getElementById("white-board").components["drawable-canvas"];
-const colorPickers = document.getElementById("pen-tools").getElementsByClassName("color")
+const colorPickers = document.getElementById("pen-tools").getElementsByClassName("color");
 const tick = document.getElementById("tick");
 
 for (colorPicker of colorPickers) {
   if (colorPicker.attributes.color.value === whiteBoardComponents.penColor) {
-    const [x, y, z] = colorPicker.object3D.position.toArray()
-    tick.object3D.position.set(x,y,z+0.1)
+    const [x, y, z] = colorPicker.object3D.position.toArray();
+    tick.object3D.position.set(x, y, z + 0.01);
   }
-  colorPicker.object3D.addEventListener("cursordown", function(){
+  colorPicker.object3D.addEventListener("cursordown", function() {
     whiteBoardComponents.penColor = this.el.attributes.color.value;
-    tick.object3D.position.set(-0.05,-0.05,0.45)
+    const [x, y, z] = this.position.toArray();
+    tick.object3D.position.set(x, y, z + 0.01);
   });
 }
